838feceecc0e01baa3a3b789d589a0c79d82cd3a
Abner Alexon <146349299+AbnerAlexson@users.noreply.github.com>
2023-11-29 09:50:32 +0800

    added initial calculator operators

---
+++ app.js
@@ -0,0 +1,21 @@
+function addOperand(a,b) {
+    return a + b
+}  
+
+function subtractOperand(a,b) {
+    return a - b
+} 
+
+function multiplyOperand(a,b) {
+    return a * b
+} 
+
+function divideOperand(a,b) {
+    return a / b
+} 
+
+//function operate(operator, a, b) {
+  //  a = Number(a)
+ //   b = Number(b)
+ ///   switch (operator)
+//}
