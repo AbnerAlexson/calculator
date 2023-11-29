803713e158d0848b0fe1bc98e404781cb524ce9c
Abner Alexon <146349299+AbnerAlexson@users.noreply.github.com>
2023-11-29 23:34:13 +0800

    update javascript
    added functionality to the numbers button
    
    added functionality to operator buttons
    
    added functionality to display
    
    added functionality for clear button
    
    still working with delete button functionality
    
    a few things may change in the future

--- app.js
+++ app.js
@@ -1,3 +1,80 @@
+//variables
+let displayNum1 = '';
+let displayNum2 = '';
+let storeOperand = '';
+
+//button varibles
+let display = document.getElementById('display')
+let pointBtn = document.querySelector('.point')
+let clearBtn = document.querySelector('.ac')
+let deleteBtn = document.querySelector('.dlt')
+let equalBtn = document.querySelector('.equal')
+let numBtn = document.querySelectorAll('.number')
+let operandBtn = document.querySelectorAll('.operator')
+
+numBtn.forEach(numbers => {
+    numbers.addEventListener('click', () => pushNum(numbers.textContent))
+});
+operandBtn.forEach(operators =>{
+    operators.addEventListener('click', () => pushOperand(operators.textContent))
+})
+clearBtn.addEventListener('click', () => {
+    clearDisplay();
+    displayNum1 = '';
+    displayNum2 = '';
+    storeOperand = '';
+})
+deleteBtn.addEventListener('click', () => {
+
+})
+
+
+function pushNum(number) {
+    if (!displayNum1 && !storeOperand) {
+        displayNum1 += number
+        display.textContent += number
+    } else if(displayNum1 && !storeOperand) {
+        displayNum1 += number
+        display.textContent += number
+    } else if (!displayNum1 && storeOperand) {
+        displayNum1 = '0';
+        displayNum2 += number
+        display.textContent += number
+    } else if (displayNum1 && storeOperand && !displayNum2) {
+        clearDisplay()
+        displayNum2 += number
+        display.textContent += number
+    } else {
+        displayNum2 += number
+        display.textContent += number
+    };
+};
+
+function pushOperand(operator){
+    storeOperand = operator
+} 
+
+function clearNum() {
+    if (displayNum1) displayNum1 = '';
+}
+
+function clearOperand() {
+    if (storeOperand) storeOperand = '';
+}
+
+function clearDisplay() {
+    display.textContent = ''
+}
+
+function deleteNum() {
+    display.textContent = display.textContent.slice(0, -1)
+    if(displayNum1 && !storeOperand) {
+        displayNum1 = displayNum1.slice(0, -1)
+    } else if {
+        
+    }
+}
+
 function addOperand(a,b) {
     return a + b
 }  
@@ -12,7 +89,7 @@ function multiplyOperand(a,b) {
 
 function divideOperand(a,b) {
     return a / b
-} 
+}
 
 //function operate(operator, a, b) {
   //  a = Number(a)
