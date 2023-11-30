bd4592dcbc6e567161eb319c48712ac931e17991
Abner Alexon <146349299+AbnerAlexson@users.noreply.github.com>
2023-11-30 18:11:44 +0800

    update app.js
    fix numbers overflowing from the display

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let displayNum1 = '';
 let displayNum2 = '';
 let storeOperand = '';
 let equalNum = 0;
+let transferNum = 0;
 
 //button varibles
 let display = document.getElementById('display')
@@ -33,15 +34,14 @@ clearBtn.addEventListener('click', () => {
     clearAll();
 })
 deleteBtn.addEventListener('click', () => {
-    deleteNum()
+    deleteNum();
 });
 equalBtn.addEventListener('click', () => {
-    equalNum = operate(storeOperand, displayNum1, displayNum2);
-    display.textContent = formatNumber(equalNum, 1e10);
+    equal();
 })
 
 function pushNum(number) {
-    if (equalNum > 0) { 
+    if (storeOperand == '' && equalNum >= 1) { 
         clearAll() 
         displayNum1 += number
         display.textContent += number
@@ -59,9 +59,21 @@ function pushNum(number) {
 }
 
 function pushOperand(operator){
+    
     storeOperand = operator
-    console.log(storeOperand)
 } 
+function equal() {
+    equalNum = operate(storeOperand, displayNum1, displayNum2);
+    if (countDigits(equalNum) >= 4) {
+        transferNum = Number(equalNum.toFixed(0))
+    } else if (countDigits(equalNum) < 4) {
+        transferNum = Number(equalNum.toFixed(4))
+    }
+    display.textContent = formatNumber(transferNum, 1e9);
+    displayNum1 = display.textContent.toString();
+    storeOperand = ''
+    displayNum2 = ''
+}
 
 function clearNum() {
     if (displayNum1) displayNum1 = '';
@@ -82,30 +94,72 @@ function clearAll() {
 function deleteNum() {
     if (displayNum1 && !storeOperand) {
         displayNum1 = displayNum1.slice(0, -1)
-        display.textContent = display.textContent.slice(0, -1)
+        display.textContent = displayNum1
     } else if (storeOperand && displayNum2) {
         displayNum2 = displayNum2.slice(0, -1);
-        display.textContent = display.textContent.slice(0, -1)
+        display.textContent = displayNum2
     };
 }
 
+function countDigits(number) {
+    // Handle the case for zero separately
+    if (number === 0) {
+        return 1;
+    }
+
+    // Take the absolute value of the number
+    let num = Math.abs(number);
+
+    let count = 0;
+
+    // Count the digits using a loop
+    while (num >= 1) {
+        num /= 10;
+        count++;
+    }
+
+    return count;
+}
+console.log(countDigits(-12348.2))
+
 function addOperand(a,b) {
-    return parseInt(a) + parseInt(b);
+    if (b == null || b == '') {
+        b = a;
+        return parseInt(a) + parseInt(b);
+    } else {
+        return parseInt(a) + parseInt(b);
+    }
 }
 
 function subtractOperand(a,b) {
-    return parseInt(a) - parseInt(b)
-};
+    if (b == null || b == '') {
+        b = a;
+        return parseInt(a) - parseInt(b);
+    } else {
+        return parseInt(a) - parseInt(b);
+    }
+}
 
 function multiplyOperand(a,b) {
-    return parseInt(a) * parseInt(b)
-} 
+    if (b == null || b == '') {
+        b = a;
+        return parseInt(a) * parseInt(b);
+    } else {
+        return parseInt(a) * parseInt(b);
+    }
+}
 
 function divideOperand(a,b) {
-    return parseInt(a) / parseInt(b)
+    if (b == null || b == '') {
+        b = a;
+        return parseInt(a) / parseInt(b);
+    } else {
+        return parseInt(a) / parseInt(b);
+    }
 }
 
 function operate(operand, a, b) {
+    if (operand == '' && b == '')  return;
     switch (operand) {
         case '+':
         return addOperand(a,b)
@@ -114,7 +168,6 @@ function operate(operand, a, b) {
         return subtractOperand(a,b)
         break;
         case '÷':
-        if (a == '0') return 0
         return divideOperand(a,b)
         break;
         case 'x':
@@ -123,7 +176,7 @@ function operate(operand, a, b) {
 }
 
 function formatNumber(num, threshold) {
-    if (Math.abs(num) >= threshold) {
+    if (num >= threshold) {
         return num.toExponential(4);
     } else {
         return num.toString();
